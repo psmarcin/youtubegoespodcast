c08209208dbd05440e69abc700de3f670f2cd71c
psmarcin <ps@psmarcin.me>
2019-04-13 23:40:48 +0200

    feat(trending): allow cors requests

--- pkg/utils/response.go
+++ pkg/utils/response.go
@@ -19,3 +19,10 @@ func OkResponse(w http.ResponseWriter) {
 func WriteBodyResponse(w http.ResponseWriter, body string) {
 	io.WriteString(w, body)
 }
+
+func AllowCorsResponse(w http.ResponseWriter, r *http.Request) {
+	switch origin := r.Header.Get("Origin"); origin {
+	case "http://localhost:8080", "https://yt.psmarcin.dev", "https://yt.psmarcin.me":
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+	}
+}
