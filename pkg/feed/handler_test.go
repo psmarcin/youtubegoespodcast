73f79132bf1e4334b9e4fd9b0ea69adc47e03264
Marcin <psmarcin@users.noreply.github.com>
2019-05-05 18:52:21 +0200

    test: improve tests (#10)

---
+++ pkg/feed/handler_test.go
@@ -0,0 +1,39 @@
+package feed
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_handleError(t *testing.T) {
+	type args struct {
+		w   *httptest.ResponseRecorder
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want struct {
+			code int
+		}
+	}{
+		{
+			name: "Write error",
+			args: args{
+				w:   httptest.NewRecorder(),
+				err: errors.New("Bad request"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handleError(tt.args.w, tt.args.err)
+			assert.Equal(t, tt.args.w.Code, http.StatusBadRequest)
+			assert.Equal(t, tt.args.w.Body.String(), tt.args.err.Error())
+		})
+	}
+}
