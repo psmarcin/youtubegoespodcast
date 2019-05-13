324e5200ff22ed6ccdd385c0cb61d47f612db4dd
psmarcin <ps@psmarcin.me>
2019-05-13 21:38:51 +0200

    feat: omit logs from /metrics route

--- pkg/logger/middleware.go
+++ pkg/logger/middleware.go
@@ -9,7 +9,9 @@ import (
 // Middleware act as middleware and log request method and path
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logrus.Infof("[API] Request %s %s %+v", r.Method, r.RequestURI, r.Header)
+		if r.RequestURI != "/metrics" {
+			logrus.Infof("[API] Request %s %s %+v", r.Method, r.RequestURI, r.Header)
+		}
 		next.ServeHTTP(w, r)
 	})
 }
