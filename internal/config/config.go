c7f3f2790a10b82a6741174a35d18d3021194a3e
psmarcin <ps@psmarcin.me>
2020-09-19 16:54:50 +0200

    feat: add latency to router logs

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,7 +8,7 @@ import (
 
 // Config stores all data from env variables
 type Config struct {
-	ApiRouterLoggerFormat string   `env:"ROUTER_LOGGER_FORMAT" envDefault:"{\"severity\": \"INFO\", \"time\": \"${time}\", \"level\":\"info\", \"source\": \"router\", \"path\": \"${route}\", \"url\": \"${url}\", \"method\": \"${method}\"}\n"`
+	ApiRouterLoggerFormat string   `env:"ROUTER_LOGGER_FORMAT" envDefault:"{\"severity\": \"INFO\", \"time\": \"${time}\", \"level\":\"info\", \"source\": \"router\", \"path\": \"${route}\", \"url\": \"${url}\", \"method\": \"${method}\", \"latency\": \"${latency}\"}\n"`
 	AppEnv                string   `env:"APP_ENV,required" envDefault:"development"`
 	CorsOrigins           []string `env:"CORS_ORIGINS" envDefault:"http://localhost:8080" envSeparator:","`
 	FirestoreCollection   string   `env:"FIRESTORE_COLLECTION" envDefault:"dev"`
